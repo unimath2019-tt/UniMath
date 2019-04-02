fe0b9ff1017bdc3dc1eeaaf8a369bf293fa011f6
Pierre Cagne <cagne@irif.fr>
2019-04-02 17:19:47 +0200

    init

---
+++ UniMath/CategoryTheory/ModelCategories.v
@@ -0,0 +1,13 @@
+(** model categories **)
+
+
+Require Import UniMath.Foundations.Propositions.
+Require Import UniMath.Foundations.Sets.
+Require Import UniMath.MoreFoundations.Notations.
+Require Import UniMath.CategoryTheory.Core.Categories.
+
+Definition left_lifting (C: precategory) (a b: C) (f: esa --> b) := 3 .
+
+Definition llp
+
+Definition weak_factorization_system {A: category} :=
