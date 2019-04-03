37be9fd72072aa4c575e678b050a4290bb356a78
Pierre Cagne <cagne@irif.fr>
2019-04-03 10:40:59 +0200

    lifting_property

--- UniMath/CategoryTheory/ModelCategories.v
+++ UniMath/CategoryTheory/ModelCategories.v
@@ -1,13 +1,16 @@
 (** model categories **)
 
-
+Require Import UniMath.Foundations.PartD.
 Require Import UniMath.Foundations.Propositions.
 Require Import UniMath.Foundations.Sets.
 Require Import UniMath.MoreFoundations.Notations.
 Require Import UniMath.CategoryTheory.Core.Categories.
 
-Definition left_lifting (C: precategory) (a b: C) (f: esa --> b) := 3 .
+Local Open Scope cat.
 
-Definition llp
+Definition lifting_prop {C: precategory} {a b x y: C} (l: a --> b) (r: x --> y) :=
+  ∏(f: a --> x) (g: b --> y), ∑(h: b --> x), (h∘l = f) × (r∘h = g) : UU.
+
+Definition llp {C: precategory} {a b x y: C} (l: a --> b) (r: x --> y) :=
 
 Definition weak_factorization_system {A: category} :=
