5f4f6c353269681d8789e38b15e286671b0caa78
Danny Gratzer <danny.gratzer@gmail.com>
2019-04-03 13:49:54 +0200

    staging for lunch time -- proof of multisets/memberships in them

--- UniMath/MoreFoundations/GrothendieckUniverse.v
+++ UniMath/MoreFoundations/GrothendieckUniverse.v
@@ -42,9 +42,11 @@ Section nat_grothendieck_universe.
   Defined.
 
   (* A series of lemmas that we will need for the main theorem *)
-  Lemma equal_carrier_equal_hset {A B : hSet} : (A : UU) = (B : UU) → A = B.
+  Lemma equal_carrier_equal_hset {A B : hSet} : (A : UU) = (B : UU) ≃ A = B.
   Proof.
-    intros H; destruct A, B; apply total2_paths2_f with H; apply isapropisaset.
+    (* Massive overkill *)
+    rewrite (weqtopaths (hSet_univalence _ _)).
+    apply univalence.
   Defined.
 
   Lemma decode_nat_plus (a b : nat) :
@@ -140,8 +142,7 @@ Section nat_grothendieck_universe.
     iscontr  (∏ (a : ∅), X a).
   Proof.
     exists fromemptysec; intros. apply funextsec. unfold homotsec. intros; contradiction.
-  Qed.
-
+  Defined.
 
   Definition finite_pi_as_prod (a : nat) (b : decode_nat a -> UU) :
     (∏ (x : decode_nat a), b x) = iterated_prod a b.
@@ -183,7 +184,7 @@ Section gylterud_grothendieck_universe.
   (* For now, we will define this as an inductive. Later we may attempt
    * to remove this and postulate/construct it manually
    *)
-  Inductive W (A : hSet) (B : A → hSet) : UU :=
+  Inductive W (A : UU) (B : A → UU) : UU :=
   | sup : ∏ (a : A), (B a → W A B) → W A B.
   Arguments sup {_} {_} _ _.
 
@@ -196,7 +197,7 @@ Section gylterud_grothendieck_universe.
     | sup a b => b
     end.
 
-  Definition W_unfolding (A : hSet) (B : A → hSet):
+  Definition W_unfolding (A : UU) (B : A → UU):
     W A B ≃ (∑ (a : A), B a → W A B).
   Proof.
     use tpair.
@@ -219,7 +220,7 @@ Section gylterud_grothendieck_universe.
    *  https://homotopytypetheory.org/2012/09/21/positive-h-levels-are-closed-under-w/
    *)
   Definition supequalf
-    {A : hSet} {B : A → hSet} {a a' : A} {b : B a → W A B} {b' : B a' → W A B} :
+    {A} {B : A → UU} {a a' : A} {b : B a → W A B} {b' : B a' → W A B} :
     (sup a b = sup a' b') ≃
     (∑ (p : a = a'), ∏ (x : B a'), transportf (fun x => B x → W A B) p b x = b' x).
   Proof.
@@ -235,25 +236,185 @@ Section gylterud_grothendieck_universe.
     rewrite <-(weqtopaths H').
     rewrite <-(weqtopaths H).
     apply (weqonpaths (W_unfolding A B)).
-  Qed.
+  Defined.
 
-  Lemma W_isaset A B : isaset (W A B).
+  Lemma W_isofhlevel n {A : UU} {B : A → UU} :
+    isofhlevel (S n) A →
+    (∏ (a : A), isofhlevel (S n) (B a)) →
+    isofhlevel (S n) (W A B).
   Proof.
-    intros w.
+    intros hlevelA hlevelB w.
     induction w as [a b IH].
     intros [a' b'].
     rewrite (weqtopaths supequalf).
 
-    refine
-      (isaprop_total2
-         ((a = a') ,, (pr2 A a a'))
-         (fun p =>  ((∏ x : B a', transportf (λ x0 : A, B x0 → W A B) p b x = b' x) ,, _))).
-    apply impred_isaprop.
+    apply (isofhleveltotal2 n); auto.
+    intro p.
+    apply impred.
     intro c.
     rewrite <-(pathsinv0inv0 p).
     rewrite (idpath _ : transportf _ (! (! p)) b = transportb _ (! p) b).
     rewrite <-transportb_fun'.
     apply IH.
   Defined.
+
+  Definition isembedding {A B} (f : A → B) : UU :=
+    ∏ (x y : A), isweq (@maponpaths _ _ f x y).
+
+  Lemma isaprop_isembedding {A B : UU} (f : A → B) :
+    isaprop (isembedding f).
+  Proof.
+    apply impred; intros x.
+    apply impred; intros y.
+    apply isapropisweq.
+  Qed.
+
+  (* The type of multisets, NOT an hSet. *)
+  Definition M : UU := W hSet (fun A => (A : UU)).
+
+  Definition memberM (m1 m2 : M) : UU.
+  Proof.
+    induction m2 as [A El _].
+    exact (∑ (a : A), El a = m1).
+  Defined.
+
+  Definition memberM_isaset (m1 m2 : M) :
+    isaset (memberM m1 m2).
+  Proof.
+    induction m2 as [A El _].
+    refine (isaset_total2_hSet A (fun a => ((El a = m1) ,, _))).
+    set (H' := @W_isofhlevel 2 hSet (fun a => (a : UU))).
+    assert (isofhlevel 3 hSet) as hoflevel3.
+    { apply isofhlevelssn.
+      intros [A' A'_isaset].
+      rewrite <-(weqtopaths equal_carrier_equal_hset).
+      apply (isofhlevelpathspace 2 A' A'); apply isofhlevelssnset; auto.
+    }
+    apply H' in hoflevel3.
+    change isaset with (isofhlevel 2).
+    exact (hoflevel3 (El a) m1).
+    intros [? ?].
+    apply hlevelntosn.
+    change isaset with (isofhlevel 2).
+    auto.
+  Defined.
+
+  Definition itset (m : M) : hProp.
+  Proof.
+    induction m as [A El IH].
+    refine ((isembedding El × ∏ (a : A), IH a) ,, _).
+    apply isapropdirprod;
+      [apply isaprop_isembedding | apply impred; apply IH].
+  Defined.
+
+  (* The universe of sets *)
+  Definition V : UU :=  ∑ (m : M), itset m.
+
+  Definition V_isaset : isaset V.
+  Proof.
+
+
+Definition isembedding {A B : UU} (f : A → B) : UU :=
+  ∏ (x y : A), isweq (@maponpaths A B f x y).
+
+
+Inductive M : UU :=
+  sup : ∏ (A : UU) (f : A → M), M.
+
+Definition memberM (x y : M) : UU.
+Proof.
+destruct y as [a f].
+use total2.
+- apply a.
+- intros i.
+  exact (f i = x).
+Defined.
+
+Definition itset (x : M) : UU.
+Proof.
+induction x as [a f H].
+apply dirprod.
+apply (isembedding f).
+apply (∏ (i : a), H i).
+Defined.
+
+Lemma isaprop_itset (x : M) : isaprop (itset x).
+Proof.
+induction x as [a f H]; simpl.
+apply isapropdirprod.
+- apply isaprop_isembedding.
+- apply impred; intro i; apply H.
+Qed.
+
+Definition itset_predicate (x : M) : hProp :=
+  (itset x,,isaprop_itset x).
+
+Definition V : UU := ∑ (x : M), itset x.
+
+Lemma V_eq (x y : V) : (x = y) ≃ (pr1 x = pr1 y).
+Proof.
+now apply subtypeInjectivity; intros X; apply isaprop_itset.
+Qed.
+
+Lemma isasetV : isaset V.
+Admitted.
+
+Lemma asdf (A : UU) (f : A → M) (h : itset (sup A f)) : isaset A.
+Proof.
+intros x y.
+induction h as [h1 h2].
+apply (isofhlevelweqb _ (_,,h1 x y)).
+exact (isofhlevelweqf 1 (V_eq _ _) (isasetV (f x,,h2 x) (f y,,h2 y))).
+Qed.
+
+(* Definition Vprecategory : precategory. *)
+(* Proof. *)
+(* use mk_precategory. *)
+(* - use tpair. *)
+(*   + use (V,,_). *)
+(*     intros x y. *)
+(*     induction (pr1 x) as [a f _]. *)
+(*     induction (pr1 y) as [b g _]. *)
+(*     apply (a → b). *)
+(*   + split; simpl. *)
+(*     * intros x. *)
+(*       induction (pr1 x) as [a f _]; cbn. *)
+(*       apply idfun. *)
+(*     * intros x y z. *)
+(*       induction (pr1 x) as [a f _]. *)
+(*       induction (pr1 y) as [b g _]. *)
+(*       induction (pr1 z) as [c h _]; cbn. *)
+(*       intros F G w; apply (G (F w)). *)
+(* - repeat split; simpl. *)
+(*   + intros x y; cbn. *)
+(*     now induction (pr1 x); induction (pr1 y). *)
+(*   + intros x y; cbn. *)
+(*     now induction (pr1 x); induction (pr1 y). *)
+(*   + intros x y z w; cbn. *)
+(*     induction (pr1 x); induction (pr1 y); induction (pr1 z); induction (pr1 w). *)
+(*     now intros; apply funextfun. *)
+(* Defined. *)
+
+(* Lemma has_homsets_Vprecategory : has_homsets Vprecategory. *)
+(* Proof. *)
+(* intros [x1 x2] [y1 y2]. *)
+(* cbn. *)
+(* induction y1 as [b g _]. *)
+(* induction x1 as [a f _]. *)
+(* simpl. *)
+(* apply impred_isaset; intros x. *)
+(* apply (asdf _ g y2). *)
+(* Qed. *)
+
+(* Lemma InitialV : Initial Vprecategory. *)
+(* Proof. *)
+(* use mk_Initial. *)
+(* - use tpair. *)
+(*   + now apply (sup ∅). *)
+(*   + now split; intros x. *)
+(* - intros x; cbn. *)
+(*   induction (pr1 x); cbn. *)
+(*   now apply impred_iscontr. *)
+(* Defined. *)
 
 End gylterud_grothendieck_universe.
