a14cfc5a73437f52f8d8a60b15e4e4b687037d1b
Pierre Cagne <cagne@irif.fr>
2019-04-04 19:54:28 +0200

    model categories defined

--- UniMath/CategoryTheory/ModelCategories.v
+++ UniMath/CategoryTheory/ModelCategories.v
@@ -3,9 +3,14 @@
 Require Import UniMath.Foundations.PartD.
 Require Import UniMath.Foundations.Propositions.
 Require Import UniMath.Foundations.Sets.
+Require Import UniMath.Combinatorics.FiniteSets.
 Require Import UniMath.MoreFoundations.Notations.
 Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.CategoryTheory.Core.Isos.
+Require Import UniMath.CategoryTheory.limits.products.
+Require Import UniMath.CategoryTheory.limits.coproducts.
+Require Import UniMath.CategoryTheory.limits.pullbacks.
+Require Import UniMath.CategoryTheory.limits.pushouts.
 
 Local Open Scope cat.
 
@@ -13,16 +18,14 @@ Local Open Scope cat.
 Lemma non_dep_transportf_trivial {A B: UU} {a a': A} (b: B) (p: a = a'):
   transportf (λ x, B) p b = b.
 Proof.
-  induction p.
-  rewrite idpath_transportf.
-  reflexivity.
+  apply (eqtohomot (transportf_const p B)).
 Defined.
 
 Section WeakFactorisationSystem.
 
-  Context (C: precategory).
+  Context {C: precategory}.
 
-  Definition class_mors := ∏(a b: C), hsubtype (C ⟦a,b⟧).
+  Definition class_mors (C: precategory) := ∏(a b: C), hsubtype (C ⟦a,b⟧).
 
   Definition comm_square {a b x y: C} (l: a --> b) (r: x --> y) :=
     ∑(f: a --> x) (g: b --> y), r∘f = g∘l.
@@ -34,29 +37,28 @@ Section WeakFactorisationSystem.
   Definition lifting {a b x y: C} (l: a --> b) (r: x --> y) :=
     ∏(sq: comm_square l r), hfiber (lifting_to_comm_square l r) sq.
 
-  Definition llp {a b: C} (l: a --> b) (R: class_mors ) :=
+  Definition llp {a b: C} (l: a --> b) (R: class_mors C) :=
     ∏(x y: C), ∏(r : R x y), lifting l (pr1 r).
 
-  Definition rlp {x y: C} (L: class_mors) (r: x --> y) :=
+  Definition rlp {x y: C} (L: class_mors C) (r: x --> y) :=
     ∏(a b: C), ∏(l : L a b), lifting (pr1 l) r.
 
-  Definition is_left_orth (L R: class_mors) :=
+  Definition is_left_orth (L R: class_mors C) :=
     ∏(a b: C), ∏(l: a --> b), llp l R ≃ L a b l.
 
-  Definition is_right_orth (L R: class_mors) :=
+  Definition is_right_orth (L R: class_mors C) :=
     ∏(a b: C), ∏(r: a --> b), rlp L r ≃ R a b r.
 
-  Definition is_factorization_system (L R: class_mors) :=
+  Definition is_factorization_system (L R: class_mors C) :=
     ∏(a b: C), ∏(f: a --> b), ∑(c: C), ∑(l: a --> c) (r: c --> b), f = r∘l.
 
-  Definition is_weak_factorization_system  (L R: class_mors) :=
+  Definition is_weak_factorization_system  (L R: class_mors C) :=
     (is_left_orth L R)
       × (is_right_orth L R)
       × (is_factorization_system L R).
 
-
-
-  Lemma isos_llp {hs: has_homsets C} {a b: C} (α: iso a b) : ∏(x y: C), ∏(f: x --> y), lifting α f.
+  Lemma has_homsets_isos_llp (hs: has_homsets C) {a b: C} (α: iso a b) :
+    ∏(x y: C), ∏(f: x --> y), lifting α f.
   Proof.
     intros x y f. intros [X [Y p]].
     exists (X ∘ inv_from_iso α).
@@ -70,31 +72,70 @@ Section WeakFactorisationSystem.
     rewrite assoc, iso_inv_after_iso, id_left. reflexivity.
     Unshelve.
     {
-      rewrite (non_dep_transportf_trivial (inv_from_iso α · X · f) _).
+      rewrite non_dep_transportf_trivial.
       rewrite assoc'. apply iso_inv_on_right. exact p.
     }
   Defined.
 
-  (* Lemma isos_rlp {x y: C} (α: iso x y) : ∏(a b: C), ∏(f: a --> b), lifting f α. *)
-  (* Proof. *)
-  (*   intros a b f. intros X Y. intro p. *)
-  (*   exists (inv_from_iso α ∘ Y). *)
-  (*   split. *)
-  (*   - rewrite assoc. symmetry. apply iso_inv_on_left. symmetry. *)
-  (*     exact p. *)
-  (*   - rewrite assoc', iso_after_iso_inv, id_right. *)
-  (*     reflexivity. *)
-  (* Defined. *)
+  Lemma has_homsets_isos_rlp (hs: has_homsets C) {x y: C} (α: iso x y) :
+    ∏(a b: C), ∏(f: a --> b), lifting f α.
+  Proof.
+    intros a b f. intros [X [Y p]].
+    exists (inv_from_iso α ∘ Y).
+    unfold lifting_to_comm_square.
 
-  Theorem has_homsets_implies_lifting_sets :
-    has_homsets C -> ∏(a b x y: C), ∏(l:a --> b), ∏(r: x --> y),
-    ∏(f: a --> x), ∏(g: b --> y), ∏(p: r∘f = g∘l), isaset().
+    eapply total2_paths2_f. rewrite transportf_total2. simpl.
+    eapply total2_paths2_f. apply hs.
+
+    Unshelve.
+    {
+      rewrite assoc. symmetry.
+      apply iso_inv_on_left. symmetry.
+      exact p.
+    }
+
+    Unshelve.
+    {
+      rewrite non_dep_transportf_trivial.
+      rewrite assoc', iso_after_iso_inv, id_right.
+      reflexivity.
+    }
+  Defined.
+
+  Theorem has_homsets_implies_lifting_sets (hs: has_homsets C) :
+    ∏(a b x y: C), ∏(l:a --> b), ∏(r: x --> y),
+    ∏(sq: comm_square l r), isaset(hfiber (lifting_to_comm_square l r) sq).
+  Proof.
+    intros a b x y l r [X [Y p]].
+    apply isaset_hfiber. apply hs.
+    apply isaset_total2. apply hs.
+    intro X'. apply isaset_total2. apply hs.
+    intro Y'. apply isasetaprop. apply hs.
+  Defined.
 
 End WeakFactorisationSystem.
 
-Section ModelCategory.
+Section WFS_homsets.
 
-  Context (C: precategory).
+  Context {C: category}.
+
+  Lemma isos_llp {a b:C} (α: iso a b) :
+    ∏(x y: C), ∏(f: x --> y), lifting α f.
+  Proof.
+    apply (has_homsets_isos_llp (pr2 C) α).
+  Defined.
+
+  Lemma isos_rlp {x y:C} (α: iso x y) :
+    ∏(a b: C), ∏(f: a --> b), lifting f α.
+  Proof.
+    apply (has_homsets_isos_rlp (pr2 C) α).
+  Defined.
+
+End WFS_homsets.
+
+Section ModelStructure.
+
+  Context {C: precategory}.
 
   Definition two_out_of_three (W: class_mors C) :=
     ∏(a b c: C), ∏(f: a --> b) (g: b --> c),
@@ -106,10 +147,15 @@ Section ModelCategory.
     intros a b f. exact (hconj (L a b f) (R a b f)).
   Defined.
 
-  Definition model_structure :=
-    ∑(Cof Weq Fib: class_mors C),
-    (two_out_of_three Weq)
-      × (is_weak_factorization_system C (intersect Cof Weq) Fib)
-      × (is_weak_factorization_system C Cof (intersect Fib Weq)).
+End ModelStructure.
 
-End ModelCategory.
+Definition model_structure (C: precategory) :=
+  ∑(Cof Weq Fib: class_mors C),
+  (two_out_of_three Weq)
+    × (is_weak_factorization_system (intersect Cof Weq) Fib)
+    × (is_weak_factorization_system Cof (intersect Fib Weq)).
+
+Definition model_category :=
+  ∑(C: category), (∏(I:UU), (isfinite I) -> (hasProducts I C))
+                    × (∏(I:UU), (isfinite I) -> (hasCoproducts I C))
+                    × (model_structure C).
